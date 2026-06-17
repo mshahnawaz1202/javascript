aed16ae4c0138f3cd92e4191275e9271fa304f46
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-06-17 20:58:19 +0500

    strings and functions

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -1,3 +1,24 @@
 let name = 'shahnawaz'
 let email  = 'shahnawaz@gmail.com'
 console.log(`My name is ${name}, and my Email is ${email}`);
+
+
+const gameName = 'Free Fire'
+console.log(gameName);
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.search('i'));
+
+const newStr  = gameName.substring(0,4)
+console.log(newStr);
+
+
+const trimStr = "   shah  nawaz   "
+console.log(trimStr.trim());
+
+const url = 'https://flexstudent%20nu%20edu%20pk/Login'
+console.log(url.replaceAll('%20','.'));
+
+
