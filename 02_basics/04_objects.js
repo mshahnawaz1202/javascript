42416415133be2bd7613b36aab5d83072e6d1c29
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-06-18 17:41:26 +0500

    Objects Part 02

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,72 @@
+// singleton
+
+// const User  = new Object() // singleton
+const user ={} // non-singleton
+user.id = '123ms'
+user.name = "Shah Nawaz"
+user.isLoggedIn = false
+
+// console.log(user);
+
+const user1 = {
+    email : 'shah@gamil.com',
+    fullname : {
+        userfullname: {
+            firstname : 'shah',
+            lastname: 'nawaz'   
+        }
+    },
+    location : 'Chiniot',
+    age : 20,
+     isLoggedIn : false,
+    lastLoginDays: ["Monday", "Friday"]
+}
+
+// console.log(user1);
+// console.log(user1.fullname.userfullname.firstname);
+
+
+const obj1 = {1:'a', 2:'b'}
+const obj2 = {3:'c', 4:'d'}
+
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign({},obj1,obj2) // {} => gurantees results
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(Object.entries(user));
+
+console.log(user.hasOwnProperty('name'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
