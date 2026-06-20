f726a5fad24019fc9234fe25ad367ac7fc682bcf
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-06-20 14:02:02 +0500

    Project 01 : Color Scheme Switcher

---
+++ 07_Project/Color Scheme Switcher/script.js
@@ -0,0 +1,42 @@
+const buttons = document.querySelectorAll('.button')
+
+const body = document.querySelector('body')
+
+buttons.forEach( function (button){
+    button.addEventListener('click',function(e){
+        console.log(e);
+        console.log(e.target);
+        switch(e.target.id){
+            case 'grey':
+                body.style.backgroundColor = e.target.id;
+                break
+            case 'white':
+                body.style.backgroundColor = e.target.id;
+                break
+            case 'black':
+                body.style.backgroundColor = e.target.id;
+                break
+            case 'blue':
+                body.style.backgroundColor = e.target.id;
+                break
+            case 'red':
+                body.style.backgroundColor = e.target.id;
+                break
+            case 'yellow':
+                body.style.backgroundColor = e.target.id;
+                break
+            default:
+                body.style.backgroundColor = 'white'
+
+        }
+        
+    })
+});
+
+
+
+
+
+
+
+
